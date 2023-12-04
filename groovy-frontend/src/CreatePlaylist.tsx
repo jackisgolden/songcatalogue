bf2aaf914a7a0e378c71884c34989c3115d7bc05
Sahil Alhat <sahilalhat@Sahils-MacBook-Pro.local>
2023-12-03 22:02:20 -0500

    intial CreatePlaylist component

--- groovy-frontend/src/CreatePlaylist.tsx
+++ groovy-frontend/src/CreatePlaylist.tsx
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
+import './CreatePlaylist.css'; // Importing the CSS file for styling
+
+interface Playlist {
+  id: number;
+  name: string;
+  // Add other properties as needed
+}
 
 const CreatePlaylist: React.FC = () => {
   const [name, setName] = useState('');
+  const [playlists, setPlaylists] = useState<Playlist[]>([]); // Typed with Playlist interface
 
   const handleSubmit = () => {
-    console.log('Playlist Created:', name); // Replace with actual creation logic
+    console.log('Playlist Created:', name);
+    // Add logic to create the playlist
+    // Update the playlists list with the new playlist
     setName('');
   };
 
   return (
-    <div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      <button onClick={handleSubmit}>Create Playlist</button>
+    <div className="playlist-container">
+      <div className="playlist-list">
+        {playlists.map((playlist) => (
+          <div className="playlist-list-item" key={playlist.id}>
+            {playlist.name} {/* Using the name property from the Playlist type */}
+          </div>
+        ))}
+      </div>
+      <div className="playlist-create-form">
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="New Playlist Name"
+        />
+        <button onClick={handleSubmit}>Create Playlist</button>
+      </div>
     </div>
   );
 };
